84c08e35ab0dcf6d5b0cb8fb447d0c59e2990040
Md Kamran Shamim <kamranshamim45@gmail.com>
2025-05-17 01:30:45 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()]
+  base: '/PuzzleGame/', // 👈 This fixes the blank white screen
+  plugins: [react()],
 });
